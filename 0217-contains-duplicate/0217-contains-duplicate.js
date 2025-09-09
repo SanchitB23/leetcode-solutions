bc1ad47ed9bede57d3d566f6ff153b53903f8976
Sanchit Bhatnagar <24359070+SanchitB23@users.noreply.github.com>
2025-09-10 02:08:28 +0530

    0217-CONTAINS-DUPLICATE Stats: Time: 34 ms (26.53%), Space: 75.8 MB (32.07%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,7 +2,12 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    const set = new Set(nums);
-    return set.size !== nums.length
+var containsDuplicate = function (nums) {
+    const map = {}
+    for (let i = 0; i < nums.length; i++) {
+        const n = nums[i]
+        if (map.hasOwnProperty(n)) return true;
+        else map[n] = true
+    }
+    return false;
 };
