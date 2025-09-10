73168381ef8f1e21251c9a8828a279ed3bc99e58
Sanchit Bhatnagar <24359070+SanchitB23@users.noreply.github.com>
2025-09-10 13:14:40 +0530

    0242-VALID-ANAGRAM Stats: Time: 10 ms (82.53%), Space: 55.7 MB (90.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,15 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    return s.split('').sort().join('') === t.split('').sort().join('')
+    if (s.length !== t.length) return false;
+    const sm = {}
+    const tm = {}
+    for (i = 0; i < s.length; i++) {
+        const si = s[i], ti = t[i];
+        sm[si] = (sm[si] ?? 0) + 1
+        tm[ti] = (tm[ti] ?? 0) + 1
+    }
+    return !Object.entries(sm).filter(([k, v]) =>
+        tm[k] !== v
+    ).length
 };
