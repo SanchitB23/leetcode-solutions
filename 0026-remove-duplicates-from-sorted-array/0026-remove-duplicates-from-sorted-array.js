7b891c0d10360d3399327fbd4d46afbdd9ad23fc
Sanchit Bhatnagar <24359070+SanchitB23@users.noreply.github.com>
2025-09-10 23:40:05 +0530

    0026-REMOVE-DUPLICATES-FROM-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 58.2 MB (34.99%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,10 +5,8 @@
 var removeDuplicates = function (nums) {
     let i = j = 0;
     while (j < nums.length) {
-        if (nums[i] !== nums[j]) {
-            i++;
-            nums[i] = nums[j]
-        }
+        if (nums[i] !== nums[j]) 
+            nums[++i] = nums[j]
         j++
     }
     return i+1
