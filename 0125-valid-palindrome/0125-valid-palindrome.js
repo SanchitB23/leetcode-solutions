93f3cf284a379b5ae75a3bcdbd01e9b00da6f31a
Sanchit Bhatnagar <24359070+SanchitB23@users.noreply.github.com>
2026-02-17 21:25:52 +0530

    0125-VALID-PALINDROME Stats: Time: 8 ms (28.84%), Space: 56.6 MB (81.45%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    cs = s.toLowerCase().replace(/[^a-z0-9]/g, "")
-    let i = 0, j = cs.length - 1
+    const cleanStr = s.toLowerCase().replace(/[^a-z0-9]/g, "")
+    let i = 0, j = cleanStr.length - 1;
     while (i < j) {
-        if (cs[i] !== cs[j]) return false;
+        if (cleanStr[i] !== cleanStr[j]) return false;
         i++; j--;
     }
-    return true;
+    return true
 };
