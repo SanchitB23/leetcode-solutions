feaff5013cd95360e04df003a9a8cc8a98703835
Sanchit Bhatnagar <24359070+SanchitB23@users.noreply.github.com>
2026-02-26 02:56:36 +0530

    1458-SORT-INTEGERS-BY-THE-NUMBER-OF-1-BITS Stats: Time: 55 ms (18.64%), Space: 62.8 MB (6.78%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.js
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.js
@@ -3,18 +3,10 @@
  * @return {number[]}
  */
 var sortByBits = function (arr) {
-    const map = new Map()
-    let c = 0
-    for (n of arr) {
-        const bit = n.toString(2)
-        const len1 = [...bit].filter(e => e == "1").length;
-        if (map.has(len1)) map.set(len1, [...map.get(len1), n]);
-        else map.set(len1, [n])
-        c = Math.max(c, len1)
-    }
-    let bucket = new Array(c)
-    map.forEach((v, k) => {
-        bucket[k] = v.sort((a, b) => a - b)
+    return arr.sort((a, b) => {
+        const ba = [...a.toString(2)].filter(e => e == "1").length;
+        const bb = [...b.toString(2)].filter(e => e == "1").length;
+        if (ba !== bb) return ba - bb
+        return a - b
     })
-    return bucket.flat()
 };
